824695bdf2c9383b87c5018dbda737da00bf89c2
Grefel Nable <felnable@gmail.com>
2022-09-13 14:29:37 -0400

    Add navbar and input fields

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import "./App.css"
+import Container from "react-bootstrap/Container"
+import Navbar from "react-bootstrap/Navbar"
+import FloatingLabel from "react-bootstrap/FloatingLabel"
+import Form from "react-bootstrap/Form"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">🏋🏽‍♂️Workout Log</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <br />
+      {/* Workout inputs */}
+      <div className="input__container">
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Workout Name"
+          className="mb-3"
+        >
+          <Form.Control type="text" placeholder="Workout name" />
+        </FloatingLabel>
+        <FloatingLabel
+          controlId="floatingPassword"
+          label="Sets"
+          className="mb-3"
+        >
+          <Form.Control type="number" placeholder="Sets" />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingInput" label="Reps for Each Set">
+          <Form.Control type="number" placeholder="Reps for Each Set" />
+        </FloatingLabel>
+      </div>
+      {/* end of Workout inputs */}
+    </>
+  )
 }
 
 export default App
