3cbcf714a83e6b3eb84fb5531188f45f00acfe37
Grefel Nable <felnable@gmail.com>
2022-09-14 18:01:32 -0400

    Move table section to own js file
    Workoutlist is the table to display all workouts

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import "./App.css";
 import AddWorkout from "./components/AddWorkout";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import Button from "react-bootstrap/Button";
-import Table from "react-bootstrap/Table";
+import WorkoutList from "./components/WorkoutList";
 
 function App() {
   return (
@@ -16,45 +15,7 @@ function App() {
       <br />
       <AddWorkout />
       {/* table */}
-      <Container>
-        <Table striped bordered hover variant="dark" className="text-center">
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Workout Name</th>
-              <th>Sets</th>
-              <th>Reps</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>September 13</td>
-              <td>Bench Press</td>
-              <td>4</td>
-              <td>12</td>
-              <td>
-                <div className="btn-container">
-                  <Button variant="success">Edit</Button>
-                  <Button variant="danger">Delete</Button>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>September 13</td>
-              <td>Bicep Curl</td>
-              <td>4</td>
-              <td>12</td>
-              <td>
-                <div className="btn-container">
-                  <Button variant="success">Edit</Button>
-                  <Button variant="danger">Delete</Button>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </Table>
-      </Container>
+      <WorkoutList />
     </>
   );
 }
