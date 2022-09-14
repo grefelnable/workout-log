9153a8f09fbcaf0a50e88a91033342a0bb81d23f
Grefel Nable <felnable@gmail.com>
2022-09-14 07:07:09 -0400

    Create workout services file
    Make class function for CRUD operation

---
+++ src/services/workout.services.js
@@ -0,0 +1,42 @@
+import { db } from "../firebase-config";
+
+import {
+  collection,
+  getDocs,
+  getDoc,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+
+const workoutCollectionRef = collection(db, "workout");
+class WorkoutDataService {
+  addWorkout = (newWorkout) => {
+    //Add new workout
+    return addDoc(workoutCollectionRef, newWorkout);
+  };
+  //Update workout
+  updateWorkout = (id, updatedWorkout) => {
+    //Variable to check if the particular data in the firestore exists or not.
+    const workoutDoc = doc(db, "workout", id); //first param: firestore reference w/c is the db, second param: name of the collection, third param: id
+    return updateDoc(workoutDoc, updatedWorkout);
+  };
+  //Delete workout log
+  deleteWorkout = (id) => {
+    //Variable to check if the particular data in the firestore exists or not.
+    const workoutDoc = doc(db, "workout", id);
+    return deleteDoc(workoutDoc);
+  };
+  //Getting all workout logs
+  getAllWorkout = () => {
+    return getDocs(workoutCollectionRef);
+  };
+  //Getting specific workout
+  getWorkout = (id) => {
+    const workoutDoc = doc(db, "books", id);
+    return getDoc(workoutDoc);
+  };
+}
+
+export default new WorkoutDataService();
