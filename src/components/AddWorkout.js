cecdc3a0b2f1d9ce564145404f3e1804c7c66f1b
Grefel Nable <felnable@gmail.com>
2022-09-14 18:00:08 -0400

    Refactor import react and useState

--- src/components/AddWorkout.js
+++ src/components/AddWorkout.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
